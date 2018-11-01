026e63eb560c79032eb642a67754c452c1c1c450
Gustav Pursche <pursche@posteo.de>
2018-11-01 22:17:20 +0100

    Transform real persons tree into nodes and linkss

--- src/components/network/data.js
+++ src/components/network/data.js
@@ -1,36 +1,74 @@
-const nodes = [
-  {name: 'Hosni Mubarak', role: 'President', root: true, image: 'https://trello-attachments.s3.amazonaws.com/5bc0b20255411e16156e4aa5/5bc9d76063f97a3fe8733a54/8f82123344a63ca65a698acd5559380f/mubarak-frei-sw2-dklblau.png'},
-  {name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
-  {name: 'Alaa Mubarak', role: 'Son', connection: 'family'},
-  {name: 'Suzanne Mubarak', role: 'Spouse', connection: 'corporate'},
-  {name: 'Gamal Mubarak', role: 'Son', connection: 'corporate'},
-  {name: 'Alaa Mubarak', role: 'Son', connection: 'government'},
-  {name: 'Suzanne Mubarak', role: 'Spouse', connection: 'government'},
-  {name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
-  {name: 'Alaa Mubarak', role: 'Son', connection: 'corporate'},
-  {name: 'Suzanne Mubarak', role: 'Spouse', connection: 'government'},
-  {name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
-  {name: 'Alaa Mubarak', role: 'Son', connection: 'family'},
-  {name: 'Suzanne Mubarak', role: 'Spouse', connection: 'family'},
-  {name: 'Omar Alaa Mubarak', role: 'Grandson', connection: 'family'},
-  {name: 'Mahmoud Gamal Mubarak', role: 'Grandson', connection: 'government'}
+import * as d3 from 'd3';
+
+const persons = [
+  {
+    id: 1,
+    name: 'Hosni Mubarak',
+    role: 'President',
+    image: 'https://trello-attachments.s3.amazonaws.com/5bc0b20255411e16156e4aa5/5bc9d76063f97a3fe8733a54/8f82123344a63ca65a698acd5559380f/mubarak-frei-sw2-dklblau.png',
+    root: true,
+    network: [
+      {id: 2, name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
+      {id: 3, name: 'Alaa Mubarak', role: 'Son', connection: 'family'},
+      {id: 4, name: 'Suzanne Mubarak', role: 'Spouse', connection: 'corporate'},
+      {id: 5, name: 'Gamal Mubarak', role: 'Son', connection: 'corporate'},
+      {id: 6, name: 'Alaa Mubarak', role: 'Son', connection: 'government'},
+      {id: 7, name: 'Suzanne Mubarak', role: 'Spouse', connection: 'government'},
+      {id: 8, name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
+      {id: 9, name: 'Alaa Mubarak', role: 'Son', connection: 'corporate'},
+      {id: 10, name: 'Suzanne Mubarak', role: 'Spouse', connection: 'government'},
+      {id: 11, name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
+      {id: 12, name: 'Alaa Mubarak', role: 'Son', connection: 'family'},
+      {
+        id: 13,
+        name: 'Suzanne Mubarak',
+        role: 'Spouse 2',
+        connection: 'family',
+        network: [
+          {id: 14, name: 'sfsdf Mubarak', role: 'Spouse', connection: 'government'},
+          {id: 15, name: 'Gamal Mubarak', role: 'Son', connection: 'family'},
+        ]
+      },
+    ]
+  }
 ];
 
-const links = [
-  {source: 1, target: 0},
-  {source: 2, target: 0},
-  {source: 3, target: 0},
-  {source: 4, target: 0},
-  {source: 5, target: 0},
-  {source: 6, target: 0},
-  {source: 7, target: 0},
-  {source: 8, target: 0},
-  {source: 9, target: 0},
-  {source: 10, target: 0},
-  {source: 11, target: 0},
-  {source: 12, target: 0},
-  {source: 13, target: 0},
-  {source: 14, target: 13},
-];
+const getNodes = person => {
+  const nodes = [];
+  const links = [];
+
+  const storePerson = p => {
+    const { network, ...rest } = p;
+    nodes.push({...rest});
+  };
+
+  const gimmePeople = p => {
+    storePerson(p);
+
+    if (p.network) {
+      p.network.forEach(_ => {
+        gimmePeople(_);
+        links.push({
+          target: _.id,
+          source: p.id
+        });
+      });
+    }
+  }
+
+  gimmePeople(person);
+
+  return { nodes, links };
+};
+
+const { nodes, links } = getNodes(persons[0]);
+
+var nodeById = d3.map();
+
+nodes.forEach(_ => nodeById.set(_.id, _));
+links.forEach(_ => {
+  _.source = nodeById.get(_.source);
+  _.target = nodeById.get(_.target);
+});
 
 export { nodes, links };
