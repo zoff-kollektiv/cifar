12e6633fc512fa1fa0b7b6bc133c477db0ecc83e
Gustav Pursche <pursche@posteo.de>
2018-12-05 08:25:26 +0100

    Footer: add content

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,6 +8,33 @@ export default () => (
   <footer>
     <style jsx>{styles}</style>
 
-    <Constraint>Imprint</Constraint>
+    <Constraint>
+      <h4>
+        <a href="https://cifar.eu/impressum/">Imprint</a>
+      </h4>
+
+      <ul className="columns">
+        <li>
+          <span className="label">Editorial</span>
+          <a href="https://cifar.eu/" className="link">
+            cifar.eu
+          </a>
+        </li>
+
+        <li>
+          <span className="label">Design &amp; Development</span>
+          <a href="https://zoff-kollektiv.net/" className="link">
+            Zoff
+          </a>
+        </li>
+
+        <li>
+          <span className="label">Contact</span>
+          <a href="mailto:info@cifar.eu" className="link">
+            info@cifar.eu
+          </a>
+        </li>
+      </ul>
+    </Constraint>
   </footer>
 );
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -9,4 +9,47 @@ export default css`
     padding-bottom: 6rem;
     padding-top: 2rem;
   }
+
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    letter-spacing: 0.075rem;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.125rem;
+    margin-top: 2rem;
+    width: 50%;
+  }
+
+  .label {
+    display: block;
+    font-weight: 700;
+    margin-bottom: 0.3rem;
+  }
+
+  .link {
+    font-weight: 400;
+  }
+
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
+
+  a:hover,
+  a:focus {
+    text-decoration: underline;
+  }
 `;
