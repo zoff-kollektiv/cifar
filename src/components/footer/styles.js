c64dcae04d770840a0d95751322f8e8b1e06ec07
Gustav Pursche <pursche@posteo.de>
2018-12-09 20:45:23 +0100

    Footer: improve styles on small screens

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -2,26 +2,40 @@ import css from 'styled-jsx/css';
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-import { colors } from '../../tokens';
+import { colors, mq } from '../../tokens';
 
 export default css`
   footer {
     background-color: ${colors.blue};
     color: white;
-    padding-bottom: 6rem;
-    padding-top: 2rem;
+    padding-bottom: 4rem;
+    padding-top: 1rem;
+  }
+
+  @media ${mq.phone} {
+    footer {
+      padding-bottom: 6rem;
+      padding-top: 2rem;
+    }
   }
 
   h4 {
-    font-size: 1.5rem;
+    font-size: 1rem;
     font-weight: 700;
     letter-spacing: 0.075rem;
+    margin-bottom: 0;
   }
 
   @supports (font-variation-settings: normal) {
     .h4 {
       font-variation-settings: 'wght' 700;
       font-weight: inherit;
+    }
+  }
+
+  @media ${mq.phone} {
+    h4 {
+      font-size: 1.5rem;
     }
   }
 
@@ -41,9 +55,16 @@ export default css`
   .column {
     display: flex;
     flex-direction: column;
-    font-size: 1.125rem;
-    margin-top: 2rem;
+    font-size: 0.8rem;
+    margin-top: 1.5rem;
     width: 50%;
+  }
+
+  @media ${mq.phone} {
+    .column {
+      font-size: 1.125rem;
+      margin-top: 2rem;
+    }
   }
 
   .column--logo {
@@ -88,8 +109,15 @@ export default css`
 export const logoStyles = css.resolve`
   svg {
     color: white;
-    height: 8rem;
-    width: 10rem;
+    height: 4rem;
+    width: 6rem;
+  }
+
+  @media ${mq.phone} {
+    svg {
+      height: 8rem;
+      width: 10rem;
+    }
   }
 
   path {
