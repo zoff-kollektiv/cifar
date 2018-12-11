48207ce3de50980eafc573519e20c6ee431ec569
Gustav Pursche <pursche@posteo.de>
2018-12-11 17:53:49 +0100

    News: include more categories

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,16 @@ const remarkHtml = require('remark-html');
 const slugify = require('slugify');
 
 /**
- * Categories:
+ * Categories
+ *
+ * 22: Tunisia
+ * 27: Egypt
+ * 46: Ukraine
  * 66: sanctions
  */
 
 const POSTS_WP_ENDPOINT =
-  'https://cifar.eu/wp-json/wp/v2/posts/?per_page=3&categories=66';
+  'https://cifar.eu/wp-json/wp/v2/posts/?per_page=3&categories=22,27,46,66';
 
 const createSlug = text =>
   slugify(text, {
