aed4883989148863615f78ccc3e98a05499a831b
Gustav Pursche <pursche@posteo.de>
2018-12-20 08:57:57 +0100

    Update favicon

--- src/components/with-layout/index.jsx
+++ src/components/with-layout/index.jsx
@@ -6,7 +6,23 @@ import styles from './styles';
 export default Wrapped => props => (
   <Fragment>
     <Helmet>
-      <link rel="icon" type="image/jpeg" href="/favicon.jpg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
     </Helmet>
 
     <style jsx>{styles}</style>
