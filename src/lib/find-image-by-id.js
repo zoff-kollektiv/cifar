51e48f434c67a31839fdf29fc7d8e5f8179edbbb
Gustav Pursche <pursche@posteo.de>
2019-03-06 11:21:03 +0100

    Network: fix default image for Mubarak

--- src/lib/find-image-by-id.js
+++ src/lib/find-image-by-id.js
@@ -1,9 +1,15 @@
-export default (images, id) =>
-  images.find(({ node: { parent: { absolutePath } } }) => {
+export default (images, id) => {
+  const image = images.find(({ node: { parent: { absolutePath } } }) => {
     const normalizedId = id >= 10 ? id : `0${id}`;
+    return absolutePath.includes(`/media/${normalizedId}-`);
+  });
 
-    return (
-      absolutePath.includes(`/${normalizedId}-`) ||
+  // if no image was found, return the default avatar
+  if (!image) {
+    return images.find(({ node: { parent: { absolutePath } } }) =>
       absolutePath.includes(`/media/avatar`)
     );
-  });
+  }
+
+  return image;
+};
